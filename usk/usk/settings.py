a52cefed1e13f06802d625f5bf71e22fd091f303
morsk <khalidfour@yahoo.com>
2023-08-23 06:37:45 -0500

    add authentication

--- usk/usk/settings.py
+++ usk/usk/settings.py
@@ -126,3 +126,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+LOGIN_REDIRECT_URL = '/'  # Or any path you'd like to redirect after login
